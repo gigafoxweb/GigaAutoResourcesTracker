3bf1a059ce8528ba73dafd78f2ddbb31b09448ed
gigahertz <gigahertz@syneforge.com>
2019-09-25 12:25:08 +0300

    @hotfix-herb

--- main.lua
+++ main.lua
@@ -1,20 +1,19 @@
+
 local playerUnghostFrame = CreateFrame('Frame')
 
 playerUnghostFrame:RegisterEvent("PLAYER_UNGHOST")
 playerUnghostFrame:RegisterEvent("PLAYER_ALIVE")
 
-playerUnghostFrame:SetScript("OnEvent", function (self, event, addon, ...)
-	if ((event =="PLAYER_UNGHOST") or (event == "PLAYER_ALIVE")) then
-		local findMineralsSpellID = 2580
-		local findHerbsSpellID = 2383
+playerUnghostFrame:HookScript("OnEvent", function ()
+	local findMineralsSpellID = 2580
+	local findHerbsSpellID = 2383
 
-		local miningUsable = IsUsableSpell(findMineralsSpellID);
-		local herbalismUsable = IsUsableSpell(findHerbsSpellID);
+	local miningUsable = IsSpellKnown(findMineralsSpellID);
+	local herbalismUsable = IsSpellKnown(findHerbsSpellID);
 
-		if (miningUsable) then
-			CastSpellByID(findMineralsSpellID)
-		elseif (herbalismUsable) then
-			CastSpellByID(findHerbsSpellID)
-		end
+	if (miningUsable) then
+		CastSpellByID(findMineralsSpellID)
+	elseif (herbalismUsable) then
+		CastSpellByID(findHerbsSpellID)
 	end
 end)
